6080da2a957f605797d41dec63f6fed2c251b463
大澤　和樹 <blood0328@gmail.com>
2018-02-15 12:56:14 +0900

    java「クラス-クラスの継承-」の課題提出します。

---
+++ CampChallenge_java/013.クラス/class2.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.mypackage.sample;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author GaRi0077
+ */
+@WebServlet(name = "class2", urlPatterns = {"/class2"})
+public class class2 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+           
+            class Num{
+                public String str = "";
+                public int num = 0;
+                
+                public void setName(String a, int b){
+                    this.str = a;
+                    this.num = b;
+                }
+                
+                public void out(){
+                    
+                    out.print("現在の値は<br>" + str + "<br>");
+                    out.print( num + "です。<br>");
+                }
+            }
+            
+            class Num2 extends Num{
+                void clear(){
+                     str = null;
+                     num = 0;
+                    
+                }
+            }
+            
+            Num2 object = new Num2();
+            
+            object.setName("山田　太郎",100);
+            object.out();
+            
+            out.print("「clearメソッド」を呼び出す<br>");
+            
+            object.clear();
+            object.out();
+            
+            
+            
+            
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
